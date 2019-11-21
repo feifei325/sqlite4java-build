96b3906d73fd14759055859a60c1657b72aaf558
webfolderio <support@webfolder.io>
2019-11-21 13:44:38 +0300

    [skip ci]

--- build.sh
+++ build.sh
@@ -19,10 +19,10 @@ curl -O -L https://github.com/microsoft/vcpkg/archive/master.zip
 unzip master.zip
 cd vcpkg-master
 ./bootstrap-vcpkg.sh
-vcpkg install sqlite3
+./vcpkg install sqlite3
 cd ..
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="../vcpkg-master/scripts/buildsystems/vcpkg.cmake"
 cmake --build . --target sqlite4java --config Release
-cmake -- build .
+strip libsqlite4java.so
