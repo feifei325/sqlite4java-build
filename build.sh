7afe63c9b38ad42bb5f3ab78891d8565cf1b7736
webfolderio <support@webfolder.io>
2019-11-21 13:26:42 +0300

    fixed [skip ci]

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 curl -O -L https://bitbucket.org/almworks/sqlite4java/get/7b55b3eab690.zip
-7b55b3eab690.zip
 unzip 7b55b3eab690.zip
 mkdir src/main/java/com/almworks/sqlite4java
 mkdir src/main/java/javolution/util/stripped
@@ -8,13 +7,13 @@ mkdir src/test/java/com/almworks/sqlite4java
 mkdir src/test/resources
 mkdir native
 mkdir swig/stub/com/almworks/sqlite4java
-copy almworks-sqlite4java-7b55b3eab690/java/com/almworks/sqlite4java/*.java src/main/java/com/almworks/sqlite4java
-copy almworks-sqlite4java-7b55b3eab690/java/javolution/util/stripped/*.java src/main/java/javolution/util/stripped
-copy almworks-sqlite4java-7b55b3eab690/test/com/almworks/sqlite4java/*.java src/test/java/com/almworks/sqlite4java
-copy almworks-sqlite4java-7b55b3eab690/swig/stub/com/almworks/sqlite4java/*.java src/main/java/com/almworks/sqlite4java
-copy almworks-sqlite4java-7b55b3eab690/swig/*.i swig
-copy almworks-sqlite4java-7b55b3eab690/swig/*.h swig
-copy almworks-sqlite4java-7b55b3eab690/native native
+cp almworks-sqlite4java-7b55b3eab690/java/com/almworks/sqlite4java/*.java src/main/java/com/almworks/sqlite4java
+cp almworks-sqlite4java-7b55b3eab690/java/javolution/util/stripped/*.java src/main/java/javolution/util/stripped
+cp almworks-sqlite4java-7b55b3eab690/test/com/almworks/sqlite4java/*.java src/test/java/com/almworks/sqlite4java
+cp almworks-sqlite4java-7b55b3eab690/swig/stub/com/almworks/sqlite4java/*.java src/main/java/com/almworks/sqlite4java
+cp almworks-sqlite4java-7b55b3eab690/swig/*.i swig
+cp almworks-sqlite4java-7b55b3eab690/swig/*.h swig
+cp almworks-sqlite4java-7b55b3eab690/native native
 swig -java -package com.almworks.sqlite4java -outdir swig/stub/com/almworks/sqlite4java -o swig/sqlite_wrap.c swig/sqlite.i
 curl -O -L https://github.com/microsoft/vcpkg/archive/master.zip
 unzip master.zip
